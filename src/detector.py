fd702d37c09db16db625018b1ce950add73faf61
unknown <saragih.meyer0821@gmail.com>
2020-10-06 15:05:25 +0700

    Detector layer back propagation

--- src/detector.py
+++ src/detector.py
@@ -39,3 +39,45 @@ class Detector:
     def get_output(self, inputarray):
         self.sigma = input
         return self.activate()
+
+    
+    def back_propagation(self, delta_matrix):
+        if self.activation_function == "sigmoid":
+            ones = np.zeros(delta_matrix.shape) + 1
+            return delta_matrix * (ones - delta_matrix)
+        elif self.activation_function == "tanh":
+            ones = np.zeros(delta_matrix.shape) + 1
+            return ones - np.square(np.tanh(delta_matrix))
+        elif self.activation_function == "relu":
+            return [[1 if col > 0 else 0 for col in row] for row in delta_matrix]
+        elif self.activation_function == "leaky_relu":
+            return [[self.leaky_slope if col > 0 else 0 for col in row] for row in delta_matrix]
+
+if __name__ == "__main__":
+    print("Testing numpy detector layer")
+
+    print("Forward")
+    print("Input")
+    bias_matrix = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    test_matrix = np.array([[-1, 1, 2, 4], [5, -6, 7, 8], [3, -2, 1, 0], [1, -2, -3, 4]])
+    print(test_matrix)
+    detector_layer = Detector(test_matrix)
+    detector_layer.bias = bias_matrix
+
+    detector_layer.activation_function = "relu"
+    print(detector_layer.leaky_slope)
+    forward_result = detector_layer.activate()
+
+    print("Result")
+    print(forward_result)
+
+    print("Backward")
+    print("Input")
+    test_delta = np.array([[-1, 1, 2, 4], [5, -6, 7, 8], [3, -2, 1, 0], [1, -2, -3, 4]])
+    print(test_delta)
+    
+    backward_result = detector_layer.back_propagation(test_delta)
+    print("Result")
+    print(backward_result)
+
+
