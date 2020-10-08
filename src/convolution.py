+ fix conv backprop

src/convolution.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 class Convolution:
-    def __init__(self, image = None, paddingSize = 2, filterSizeH = 3, filterSizeW = 3, strideSize = 1, filters = None):
+    def __init__(self, batchsize, batchperepoch, image = None, paddingSize = 2, filterSizeH = 3, filterSizeW = 3, strideSize = 1, filters = None):
         self.paddingSize = paddingSize
         self.strideSize = strideSize
         self.filterSizeH = filterSizeH
         self.filterSizeW = filterSizeW
+
+        self.batchsize = batchsize
+        self.batchperepoch = batchperepoch
+        self.cache = False
+        self.deltaweights = None
 
         if image is not None:
             self.image = np.transpose(image,(2, 0, 1))
@@ -104,7 +109,7 @@ class Convolution:
                 if (region.shape[0] == sizeH and region.shape[1] == sizeW):
                     yield region, i, j
 
-    def back_propagation(self, delta_matrix, learning_rate):
+    def back_propagation(self, delta_matrix):
         print('CONV back_propagation')
         print('delta_matrix:')
         print(delta_matrix)
@@ -123,11 +128,22 @@ class Convolution:
                 print(i, j, '-->', curr_region)
                 weight += delta_matrix[i, j] * curr_region
 
+        if self.cache:
+            self.deltaweights = self.deltaweights + weight
+        else:
+            self.deltaweights = weight
+            self.cache = True
+
         print('convolution weight:')
-        print(weight)
+        print(self.deltaweights)
 
-        totalWeight = weight * learning_rate
-        print('convolution totalWeight:')
-        print(totalWeight)
+        return self.deltaweights
 
-        return totalWeight
+    def updateWeight(self, learningrate):
+        self.cache = False
+        self.deltaweights = (self.deltaweights / (self.batchsize * self.batchperepoch)) * learningrate
+
+        print('convolution weight:')
+        print(self.deltaweights)
+
+        return self.deltaweights