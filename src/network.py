bb8e63d1204cb7648b4e69b7be86664589d5a34f
rayzamgh <13517073@std.stei.itb.ac.id>
2020-10-08 23:18:02 +0700

    SAVE LOAD MODEL HAMPIR BERES, BATCHING BELOM REEEEEEEEEE

--- src/network.py
+++ src/network.py
@@ -32,9 +32,9 @@ class Network:
         self.output_layer.initiateLayer()
 
     def train_one(self, fileName, label):
-        ##################################
-        #IMAGE LOAD & FORWARD PROPAGATION#
-        ##################################
+        ####################################
+        # IMAGE LOAD & FORWARD PROPAGATION #
+        ####################################
         extractedImage = extractImage(fileName, True, self.convInputSize, self.convInputSize)
         self.convolution_layer.setInputs(np.array(extractedImage[0]))
         self.convolution_layer.convolutionForward()
@@ -45,9 +45,9 @@ class Network:
         self.output_layer.executeDenseLayer(self.dense_layer.outputs)
 
         ######################################
-        #BACKWARD PROPAGATION                #
-        #Consensus Output Node 0 = Cat       #
-        #Consensus Output Node 1 = Dog       #
+        #       BACKWARD PROPAGATION         #
+        #  Consensus Output Node 0 = Cat     #
+        #  Consensus Output Node 1 = Dog     #
         ######################################
 
         d_out = self.output_layer.calcBackwards(label)
@@ -57,36 +57,46 @@ class Network:
         self.output_layer.updateWeight(learning_rate)
         self.dense_layer.updateWeight(learning_rate)
 
-    def train(self, directory, label, epoch, batchsize, batchperepoch, learning_rate):
+    def train(self, directory, label, epoch, learning_rate, val_data, train_data):
+
+        for _ in range(epoch):
+            for img in train_data:
+                self.train_one(img, label)
+                print("TRAIN DONE : ", img)
+            self.update_weight(learning_rate)
+
+    def kfoldxvalidation(self, directory, label, epoch, learning_rate):
+
+        listimg = []
 
         if directory:
             os.chdir(directory)
 
         files = os.listdir()
 
-        images = [file for file in files if file.endswith(('jpg'))]
+        images = ([file for file in files if file.endswith(('jpg'))])
 
-        currentbatch = []
-        for image in images:
-            currentbatch.append(image)
-            if (len(currentbatch) == batchsize):
-                for img in currentbatch:
-                    self.train_one(img, label)
-                    print("TRAIN DONE : ", img)
-                self.update_weight(learning_rate)
-                print("UPDATE WEIGHT")
-                currentbatch = []
 
-        if (len(currentbatch) > 0):
-            for img in currentbatch:
-                self.train_one(img, label)
-                print("TRAIN DONE : ", img)
-            self.update_weight(learning_rate)
-            print("UPDATE WEIGHT")
+        datas = (np.array_split(images, epoch))
+    
+        for img in datas:
+            listimg.append(list(img))
+
+        #KFOLDLOOP
+        for img in listimg:
+            datacopy = listimg[:]
+            datacopy.remove(img)
+
+            flattened_datacopy = [y for x in datacopy for y in x]
+
+            self.train(directory, label, epoch, learning_rate, img, flattened_datacopy)
+
+
 
 
 if __name__ == '__main__':
     curNetwork = Network()
-    curNetwork.initiate_network(100, 2, 3, 2, 1, 3, 1, 'AVG')
-    curNetwork.train("test_data\cats", label=0, epoch=10, batchsize=4, batchperepoch=1, learning_rate=0.001)
-    #curNetwork.train_one("src\data\hololive29.jpg", 0)
+    curNetwork.kfoldxvalidation("test_data\cats", label=0, epoch=10, learning_rate=0.001)
+    # curNetwork.initiate_network(100, 2, 3, 2, 1, 'relu', 3, 1, 'AVG')
+    # curNetwork.train("test_data\cats", label=0, epoch=10,  learning_rate=0.001)
+    # #curNetwork.train_one("src\data\hololive29.jpg", 0)
