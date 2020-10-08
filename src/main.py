9ca2bfb164af03cc3bd2080d97ddc64fe6b169df
rayzamgh <13517073@std.stei.itb.ac.id>
2020-10-08 23:18:32 +0700

    SAVE LOAD MODEL HAMPIR BERES, BATCHING BELOM REEEEEEEEEE

--- src/main.py
+++ src/main.py
@@ -88,7 +88,7 @@ def loadmodel():
 if __name__ == '__main__':
     curNetwork = Network()
     curNetwork.initiate_network(100, 2, 3, 2, 1, 'relu', 3, 1, 'AVG')
-    curNetwork.train_one("src\data\hololive29.jpg", 0)
+    curNetwork.kfoldxvalidation("test_data\cats", label=0, epoch=10, learning_rate=0.001)
 
     savemodel(curNetwork)
     loadedNetwork = loadmodel()
